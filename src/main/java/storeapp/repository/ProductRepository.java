ac17fb559f04ec6a893b8b66b2611f82204402b2
Bernardo A Mira García <68155136+bamg359@users.noreply.github.com>
2026-04-11 11:37:39 -0500

    feat: mys-4 crear array

--- src/main/java/storeapp/repository/ProductRepository.java
+++ src/main/java/storeapp/repository/ProductRepository.java
@@ -1,4 +1,22 @@
 package storeapp.repository;
 
+import storeapp.domain.Category;
+import storeapp.domain.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ProductRepository {
+
+
+    List<Product> products = new ArrayList<>(
+            Arrays.asList(
+                    new Product(1, "Laptop", 999.99,50,true, new Category(1, "Electronics", true))
+
+            )
+    );
+
+
+
 }
